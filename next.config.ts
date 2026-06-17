30721571f9adbe0aba81b3fed71908e33549bf64
Trần Vũ <124017908+tranvuxdev@users.noreply.github.com>
2026-06-17 19:51:04 +0700

    refactor: remove tailwindcss-mangle configuration and update dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
-import utwm from "unplugin-tailwindcss-mangle/webpack";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  webpack: (config, { dev }) => {
-    // Only scramble classnames during production builds
-    if (!dev) {
-      config.plugins.push(utwm());
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
